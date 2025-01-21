1d380657527f1664f1ce48a0f94a9b6cc4bfe71f
Sören Maschmann <smaschmann@slashwhy.de>
2025-01-21 16:05:01 +0100

    fix(header): update command input style

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -57,7 +57,7 @@
     class="{section({
       x: false,
       y: false,
-    })} bg-layer/70 flex items-center rounded-lg border p-4 shadow-lg backdrop-blur-lg"
+    })} flex items-center rounded-lg border bg-layer/70 p-4 shadow-lg backdrop-blur-lg"
   >
     <Button.Root
       class={button({ type: 'icon', kind: 'ghost' })}
@@ -91,18 +91,18 @@
             <span class=" hidden sm:block">
               {#if os.name === OPERATING_SYSTEMS[0]}
                 <kbd
-                  class="rounded-xs inline-flex size-6 items-center justify-center border bg-background"
+                  class="inline-flex size-6 items-center justify-center rounded-xs border bg-background"
                   >⌘</kbd
                 >
               {/if}
               {#if os.name === OPERATING_SYSTEMS[1] || os.name === OPERATING_SYSTEMS[2]}
                 <kbd
-                  class="rounded-xs inline-flex size-6 items-center justify-center border bg-background"
+                  class="inline-flex size-6 items-center justify-center rounded-xs border bg-background"
                   >Ctrl</kbd
                 >
               {/if}
               <kbd
-                class="rounded-xs inline-flex size-6 items-center justify-center border bg-background"
+                class="inline-flex size-6 items-center justify-center rounded-xs border bg-background"
                 >K</kbd
               >
             </span>
@@ -117,7 +117,7 @@
           />
 
           <Dialog.Content
-            class="bg-layer fixed left-1/2 top-4 z-50 min-h-64 w-[theme(screens.sm)] max-w-[calc(100%-theme(spacing.4))] -translate-x-1/2 rounded-lg border outline-none"
+            class="fixed left-1/2 top-0 z-50 w-full max-w-screen-sm -translate-x-1/2 p-4 outline-none"
             transition={fade}
             transitionConfig={{ duration: 150 }}
           >
@@ -128,156 +128,167 @@
               >This dialog allows you to search all the content of the Shapez
               Vortex community-platform.</Dialog.Description
             >
-            <Dialog.Close
-              class="{button({
-                type: 'icon',
-                kind: 'outline',
-                size: 'sm',
-              })} absolute right-4 top-4 size-8 rounded-full border"
-            >
-              <span class="icon-[tabler--x] text-lg">Close search dialog</span>
-            </Dialog.Close>
 
-            <Command.Root label="Search" loop>
-              <div class="flex items-center gap-2 border-b py-4 pl-4 pr-16">
-                <span class="icon-[tabler--search]" />
-                <Command.Input
-                  class="h-8 w-full bg-transparent outline-none placeholder:text-muted"
-                  placeholder="Search the vortex..."
-                  bind:el={searchInputElement}
-                  bind:value={searchValue}
-                />
-              </div>
-
-              <Command.List
-                class="px-1 py-4 [&_[data-cmdk-group]+[data-cmdk-group]]:mt-4"
+            <div class="">
+              <Command.Root
+                class="relative rounded-lg border bg-layer"
+                label="Search"
+                loop
               >
-                <Command.Empty class="text-center"
-                  >No results found.</Command.Empty
+                <Dialog.Close
+                  class="{button({
+                    type: 'icon',
+                    kind: 'outline',
+                    size: 'sm',
+                  })} rounded-full absolute right-4 top-4 size-8 border"
                 >
-
-                {#if searchValue}
-                  {@const blueprints = $page.data.searchBlueprints}
-                  {#if blueprints.length > 0}
-                    <Command.Group
-                      class="[&>[data-cmdk-group-heading]]:px-3 [&>[data-cmdk-group-heading]]:text-sm [&>[data-cmdk-group-heading]]:text-muted [&>[data-cmdk-item]+[data-cmdk-item]]:mt-1"
-                      heading="Blueprints"
-                      alwaysRender
-                    >
-                      {#each blueprints as blueprint}
-                        <Command.Item
-                          class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                          id={blueprint.id}
-                          value={blueprint.title}
-                          onSelect={() =>
-                            command(() => goto(`/blueprint/${blueprint.id}`))}
-                        >
-                          <span class="icon-[tabler--schema]" />
-                          <span
-                            class="overflow-hidden text-ellipsis whitespace-nowrap"
-                          >
-                            {blueprint.title}
-                          </span>
-                          {#if blueprint.expand && blueprint.expand['creator']}
-                            <span
-                              class="rounded-xs bg-accent px-1 py-0.5 text-sm ml-auto text-accent-foreground"
-                            >
-                              @{blueprint.expand['creator'].displayname}
-                            </span>
-                          {/if}
-                        </Command.Item>
-                      {/each}
-                      <Command.Item
-                        class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                        alwaysRender
-                        onSelect={() =>
-                          command(() => searchBlueprints(searchValue))}
-                      >
-                        <span class="icon-[tabler--search]" />
-                        Show more...
-                      </Command.Item>
-                    </Command.Group>
-                  {/if}
-
-                  {@const users = $page.data.searchUsers}
-                  {#if users.length > 0}
-                    <Command.Group
-                      class="[&>[data-cmdk-group-heading]]:px-3 [&>[data-cmdk-group-heading]]:text-sm [&>[data-cmdk-group-heading]]:text-muted [&>[data-cmdk-item]+[data-cmdk-item]]:mt-1"
-                      heading="Users"
-                    >
-                      {#each users as user}
-                        <Command.Item
-                          class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                          onSelect={() =>
-                            command(() => goto(`/user/@${user.displayname}`))}
-                        >
-                          <span class="icon-[tabler--user]" />
-                          {user.displayname}
-                        </Command.Item>
-                      {/each}
-                    </Command.Group>
-                  {/if}
-                {/if}
-
-                <Command.Group
-                  class="[&>[data-cmdk-group-heading]]:px-3 [&>[data-cmdk-group-heading]]:text-sm [&>[data-cmdk-group-heading]]:text-muted [&>[data-cmdk-item]+[data-cmdk-item]]:mt-1"
-                  heading="Features"
-                >
-                  {#if isShapeIdentifier(searchValue)}
-                    <Command.Item
-                      class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                      onSelect={() =>
-                        command(() => goto(`/shape?identifier=${searchValue}`))}
-                    >
-                      <span class="icon-[tabler--stack]" />
-                      View Shape - {searchValue}
-                    </Command.Item>
-                  {:else}
-                    <Command.Item
-                      class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                      onSelect={() => command(() => goto('/shape'))}
-                    >
-                      <span class="icon-[tabler--stack]" />
-                      Shape Viewer
-                    </Command.Item>
-                  {/if}
-                  <Command.Item
-                    class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                    onSelect={() => command(() => goto('/blueprint'))}
+                  <span class="icon-[tabler--x] text-lg"
+                    >Close search dialog</span
                   >
-                    <span class="icon-[tabler--schema]" />
-                    Blueprint Viewer
-                  </Command.Item>
+                </Dialog.Close>
 
-                  {#if $page.data.user}
-                    {#if $page.data.user.verified}
-                      <Command.Item
-                        class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                        onSelect={() =>
-                          command(() => goto('/blueprint/upload'))}
+                <div class="flex items-center gap-2 border-b py-4 pl-4 pr-16">
+                  <span class="icon-[tabler--search]" />
+                  <Command.Input
+                    class="h-8 w-full bg-transparent outline-none placeholder:text-muted"
+                    placeholder="Search the vortex..."
+                    bind:el={searchInputElement}
+                    bind:value={searchValue}
+                  />
+                </div>
+
+                <Command.List
+                  class="max-h-96 overflow-y-auto px-1 py-4 [&_[data-cmdk-group]+[data-cmdk-group]]:mt-4"
+                >
+                  <Command.Empty class="text-center"
+                    >No results found.</Command.Empty
+                  >
+
+                  {#if searchValue}
+                    {@const blueprints = $page.data.searchBlueprints}
+                    {#if blueprints.length > 0}
+                      <Command.Group
+                        class="[&>[data-cmdk-group-heading]]:px-3 [&>[data-cmdk-group-heading]]:text-sm [&>[data-cmdk-group-heading]]:text-muted [&>[data-cmdk-item]+[data-cmdk-item]]:mt-1"
+                        heading="Blueprints"
+                        alwaysRender
                       >
-                        <span class="icon-[tabler--upload]" />
-                        Upload
+                        {#each blueprints as blueprint}
+                          <Command.Item
+                            class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                            id={blueprint.id}
+                            value={blueprint.title}
+                            onSelect={() =>
+                              command(() => goto(`/blueprint/${blueprint.id}`))}
+                          >
+                            <span class="icon-[tabler--schema]" />
+                            <span
+                              class="overflow-hidden text-ellipsis whitespace-nowrap"
+                            >
+                              {blueprint.title}
+                            </span>
+                            {#if blueprint.expand && blueprint.expand['creator']}
+                              <span
+                                class="ml-auto rounded-xs bg-accent px-1 py-0.5 text-sm text-accent-foreground"
+                              >
+                                @{blueprint.expand['creator'].displayname}
+                              </span>
+                            {/if}
+                          </Command.Item>
+                        {/each}
+                        <Command.Item
+                          class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                          alwaysRender
+                          onSelect={() =>
+                            command(() => searchBlueprints(searchValue))}
+                        >
+                          <span class="icon-[tabler--search]" />
+                          Show more...
+                        </Command.Item>
+                      </Command.Group>
+                    {/if}
+
+                    {@const users = $page.data.searchUsers}
+                    {#if users.length > 0}
+                      <Command.Group
+                        class="[&>[data-cmdk-group-heading]]:px-3 [&>[data-cmdk-group-heading]]:text-sm [&>[data-cmdk-group-heading]]:text-muted [&>[data-cmdk-item]+[data-cmdk-item]]:mt-1"
+                        heading="Users"
+                      >
+                        {#each users as user}
+                          <Command.Item
+                            class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                            onSelect={() =>
+                              command(() => goto(`/user/@${user.displayname}`))}
+                          >
+                            <span class="icon-[tabler--user]" />
+                            {user.displayname}
+                          </Command.Item>
+                        {/each}
+                      </Command.Group>
+                    {/if}
+                  {/if}
+
+                  <Command.Group
+                    class="[&>[data-cmdk-group-heading]]:px-3 [&>[data-cmdk-group-heading]]:text-sm [&>[data-cmdk-group-heading]]:text-muted [&>[data-cmdk-item]+[data-cmdk-item]]:mt-1"
+                    heading="Features"
+                  >
+                    {#if isShapeIdentifier(searchValue)}
+                      <Command.Item
+                        class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                        onSelect={() =>
+                          command(() =>
+                            goto(`/shape?identifier=${searchValue}`),
+                          )}
+                      >
+                        <span class="icon-[tabler--stack]" />
+                        View Shape - {searchValue}
+                      </Command.Item>
+                    {:else}
+                      <Command.Item
+                        class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                        onSelect={() => command(() => goto('/shape'))}
+                      >
+                        <span class="icon-[tabler--stack]" />
+                        Shape Viewer
                       </Command.Item>
                     {/if}
                     <Command.Item
-                      class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                      onSelect={() => command(() => goto('/user'))}
+                      class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                      onSelect={() => command(() => goto('/blueprint'))}
                     >
-                      <span class="icon-[tabler--user]" />
-                      Profile
+                      <span class="icon-[tabler--schema]" />
+                      Blueprint Viewer
                     </Command.Item>
-                    <Command.Item
-                      class="rounded-xs flex cursor-pointer items-center gap-2 px-3 py-2 transition aria-selected:bg-border"
-                      onSelect={() => command(() => goto('/settings'))}
-                    >
-                      <span class="icon-[tabler--settings]" />
-                      Settings
-                    </Command.Item>
-                  {/if}
-                </Command.Group>
-              </Command.List>
-            </Command.Root>
+
+                    {#if $page.data.user}
+                      {#if $page.data.user.verified}
+                        <Command.Item
+                          class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                          onSelect={() =>
+                            command(() => goto('/blueprint/upload'))}
+                        >
+                          <span class="icon-[tabler--upload]" />
+                          Upload
+                        </Command.Item>
+                      {/if}
+                      <Command.Item
+                        class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                        onSelect={() => command(() => goto('/user'))}
+                      >
+                        <span class="icon-[tabler--user]" />
+                        Profile
+                      </Command.Item>
+                      <Command.Item
+                        class="flex cursor-pointer items-center gap-2 rounded-xs px-3 py-2 transition aria-selected:bg-border"
+                        onSelect={() => command(() => goto('/settings'))}
+                      >
+                        <span class="icon-[tabler--settings]" />
+                        Settings
+                      </Command.Item>
+                    {/if}
+                  </Command.Group>
+                </Command.List>
+              </Command.Root>
+            </div>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
@@ -308,7 +319,7 @@
             </Menubar.Trigger>
 
             <Menubar.Content
-              class="bg-layer/70 z-20 space-y-1 rounded-lg border p-2 outline-none backdrop-blur-lg"
+              class="z-20 space-y-1 rounded-lg border bg-layer/70 p-2 outline-none backdrop-blur-lg"
               transition={blur}
               transitionConfig={{ duration: 150 }}
               side="bottom"
@@ -318,7 +329,7 @@
               strategy="fixed"
             >
               <Menubar.Item
-                class="rounded-xs flex items-center gap-2 px-4 py-1 outline-none transition hover:bg-border focus-visible:bg-border"
+                class="flex items-center gap-2 rounded-xs px-4 py-1 outline-none transition hover:bg-border focus-visible:bg-border"
                 href="/user"
                 title="Go to your user profile"
               >
@@ -326,7 +337,7 @@
                 Profile
               </Menubar.Item>
               <Menubar.Item
-                class="rounded-xs flex items-center gap-2 px-4 py-1 outline-none transition hover:bg-border focus-visible:bg-border"
+                class="flex items-center gap-2 rounded-xs px-4 py-1 outline-none transition hover:bg-border focus-visible:bg-border"
                 href="/settings"
                 title="Go to your user settings"
               >
@@ -336,7 +347,7 @@
               <Menubar.Item asChild let:builder>
                 <form action="/logout" method="post">
                   <button
-                    class="rounded-xs flex w-full items-center gap-2 px-4 py-1 outline-none transition hover:bg-border focus-visible:bg-border"
+                    class="flex w-full items-center gap-2 rounded-xs px-4 py-1 outline-none transition hover:bg-border focus-visible:bg-border"
                     title="Logout"
                     use:builder.action
                     {...builder}
