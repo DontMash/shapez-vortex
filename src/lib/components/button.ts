015defa9f694d9ea16bce513d824ede9dfd08774
Sören Maschmann <smaschmann@slashwhy.de>
2025-01-22 01:12:53 +0100

    feat: add link button variant

--- src/lib/components/button.ts
+++ src/lib/components/button.ts
@@ -5,10 +5,8 @@ export const button = cva(
     'inline-flex',
     'items-center',
     'gap-2',
-    'rounded-sm',
     'focus-visible:outline-current',
     'focus-visible:outline-2',
-    'focus-visible:outline-offset-2',
     'focus-visible:outline',
     'transition',
   ],
@@ -22,16 +20,26 @@ export const button = cva(
         fill: [],
         outline: ['border', 'hover:bg-border'],
         ghost: ['border', 'border-transparent', 'hover:bg-border'],
+        link: [
+          'hover:underline',
+          'focus-visible:underline',
+          'focus-visible:-outline-offset-4',
+        ],
       },
       intent: {
         primary: [],
       },
       size: {
-        sm: [],
-        md: [],
+        sm: ['rounded-xs'],
+        md: ['rounded-sm'],
+        lg: ['rounded-md'],
       },
     },
     compoundVariants: [
+      {
+        kind: ['fill', 'ghost', 'outline'],
+        class: ['focus-visible:outline-offset-2'],
+      },
       {
         kind: 'fill',
         intent: 'primary',
@@ -41,6 +49,16 @@ export const button = cva(
           'active:bg-primary-active',
           'focus-visible:outline-primary',
           'focus-visible:hover:outline-primary-hover',
+          'focus-visible:active:outline-primary-active',
+        ],
+      },
+      {
+        kind: 'link',
+        intent: 'primary',
+        class: [
+          'hover:text-primary',
+          'active:text-primary-active',
+          'focus-visible:hover:outline-primary',
           'focus-visible:active:outline-primary-active',
         ],
       },
@@ -55,9 +73,29 @@ export const button = cva(
         class: ['size-12', 'p-1'],
       },
       {
+        type: 'icon',
+        size: 'lg',
+        class: ['size-16', 'p-3'],
+      },
+      {
+        type: 'default',
+        size: 'sm',
+        class: ['px-2', 'py-1', 'h-12'],
+      },
+      {
         type: 'default',
         size: 'md',
         class: ['px-3', 'py-2', 'h-12'],
+      },
+      {
+        kind: 'link',
+        size: 'sm',
+        class: ['-mx-2', '-my-1'],
+      },
+      {
+        kind: 'link',
+        size: 'md',
+        class: ['-mx-3', '-my-2'],
       },
     ],
     defaultVariants: {
