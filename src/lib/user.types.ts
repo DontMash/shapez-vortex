c4f61b3e7b037b319cfdb7c8c8b33887b6ca9c60
Sören Maschmann <smaschmann@slashwhy.de>
2025-01-26 10:52:58 +0100

    refactor(user): update user schemas

--- src/lib/user.types.ts
+++ src/lib/user.types.ts
@@ -1,33 +1,26 @@
 import type { RecordModel } from 'pocketbase';
 import { z } from 'zod';
 
-export const USERNAME_MIN_LENGTH = 3;
-export const USERNAME_MAX_LENGTH = 24;
-export const USERNAME_REGEX = new RegExp(
-  `^[A-Za-z0-9_]{${USERNAME_MIN_LENGTH},${USERNAME_MAX_LENGTH}}$`,
-);
-export const PASSWORD_MIN_LENGTH = 8;
+const USERNAME_MIN_LENGTH = 3;
+const USERNAME_MAX_LENGTH = 24;
+const USERNAME_REGEX = new RegExp(`^[A-Za-z0-9_]*$`);
+const USERNAME_SCHEMA = z
+  .string()
+  .min(USERNAME_MIN_LENGTH)
+  .max(USERNAME_MAX_LENGTH)
+  .regex(USERNAME_REGEX);
+const PASSWORD_MIN_LENGTH = 8;
+const PASSWORD_SCHEMA = z.string().min(PASSWORD_MIN_LENGTH);
+
 export const USER_REGISTER_FORM_SCHEMA = z.object({
-  username: z
-    .string()
-    .min(USERNAME_MIN_LENGTH)
-    .max(USERNAME_MAX_LENGTH)
-    .regex(USERNAME_REGEX),
-  displayname: z
-    .string()
-    .min(USERNAME_MIN_LENGTH)
-    .max(USERNAME_MAX_LENGTH)
-    .regex(USERNAME_REGEX),
+  username: USERNAME_SCHEMA,
+  displayname: USERNAME_SCHEMA,
   email: z.string().email(),
-  password: z.string().min(8),
+  password: PASSWORD_SCHEMA,
 });
 export const USER_LOGIN_FORM_SCHEMA = z.object({
-  username: z
-    .string()
-    .min(USERNAME_MIN_LENGTH)
-    .max(USERNAME_MAX_LENGTH)
-    .regex(USERNAME_REGEX),
-  password: z.string().min(8),
+  username: USERNAME_SCHEMA,
+  password: PASSWORD_SCHEMA,
   redirect: z.string().optional(),
 });
 
