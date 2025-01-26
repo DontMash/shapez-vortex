da73d3a9b26326448064de68f98c3087476d4bcf
Sören Maschmann <smaschmann@slashwhy.de>
2025-01-26 22:54:07 +0100

    feat(user-bookmarks): update layout

--- src/routes/(app)/user/[slug]/bookmarks/+page.svelte
+++ src/routes/(app)/user/[slug]/bookmarks/+page.svelte
@@ -2,24 +2,20 @@
   import type { PageData } from './$types';
 
   import BlueprintItemList from '$lib/components/blueprint/BlueprintItemList.svelte';
+  import PageHeader from '$lib/components/PageHeader.svelte';
+  import { section } from '$lib/components/section';
 
   export let data: PageData;
 </script>
 
-<section class="relative mx-auto w-full max-w-5xl">
-  <header
-    class="mb-12 flex w-full items-end space-x-4 border-b border-base-content/20 px-4 pb-4"
-  >
-    <h2 class="inline-flex flex-grow items-center space-x-2 text-lg font-bold">
-      <span class="icon-[tabler--bookmark-filled] align-text-bottom text-2xl" />
-      {data.seo.title}
-    </h2>
-  </header>
+<section class={section()}>
+  <PageHeader>
+    <span class="icon-[tabler--bookmark-filled] heading-2" />
+    {data.seo.title}
+  </PageHeader>
 
   {#if data.bookmarks && data.bookmarks.length > 0}
-    <div class="px-4 lg:px-0">
-      <BlueprintItemList items={data.bookmarks} images={data.images} />
-    </div>
+    <BlueprintItemList items={data.bookmarks} images={data.images} />
   {:else}
     <div class="flex items-center justify-center">
       <span>No bookmarks</span>
