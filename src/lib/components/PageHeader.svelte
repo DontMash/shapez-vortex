cb32afed72627017777e98c817bebc981897fc43
Sören Maschmann <smaschmann@slashwhy.de>
2025-02-04 11:33:13 +0100

    feat: add description slot to page header

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,7 +1,15 @@
+<script lang="ts">
+</script>
+
 <header class="mb-12 border-b pb-4">
   <hgroup>
     <h1 class="heading-2 flex items-center gap-2">
       <slot />
     </h1>
   </hgroup>
+  {#if $$slots.description}
+    <p>
+      <slot name="description" />
+    </p>
+  {/if}
 </header>
