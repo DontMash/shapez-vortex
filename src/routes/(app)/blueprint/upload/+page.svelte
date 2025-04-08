fe05f7aa918c3d905f9dd56d699f1a2812e54340
Sören Maschmann <smaschmann@slashwhy.de>
2025-04-08 13:25:36 +0200

    feat(blueprint): update upload page header

--- src/routes/(app)/blueprint/upload/+page.svelte
+++ src/routes/(app)/blueprint/upload/+page.svelte
@@ -2,24 +2,21 @@
   import type { PageData } from './$types';
 
   import BlueprintForm from '$lib/components/blueprint/BlueprintForm.svelte';
+  import PageHeader from '$lib/components/PageHeader.svelte';
+  import { section } from '$lib/components/section';
 
   export let data: PageData;
 </script>
 
-<section class="mx-auto w-full max-w-5xl px-4 lg:px-0">
-  <header
-    class="mb-12 flex w-full items-end space-x-4 border-b border-input px-4 pb-4"
-  >
-    <hgroup>
-      <h2 class="text-lg font-bold">
-        <span class="icon-[tabler--upload] align-text-bottom text-2xl" />
-        {data.seo.title}
-      </h2>
-      <p>
-        {data.seo.description}
-      </p>
-    </hgroup>
-  </header>
+<section class={section()}>
+  <PageHeader>
+    <span class="icon-[tabler--upload] heading-2" />
+    {data.seo.title}
+
+    <svelte:fragment slot="description">
+      {data.seo.description}
+    </svelte:fragment>
+  </PageHeader>
 
   <BlueprintForm data={{ form: data.form, type: 'create', tags: data.tags }} />
 </section>
