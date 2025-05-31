53791e95cd3fb0da9099822fd8e3312781f8dddc
Sören Maschmann <smaschmann@slashwhy.de>
2025-05-31 19:31:37 +0200

    chore: upgrade testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,38 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   assetsInclude: ['**/*.vs', '**/*.fs'],
   plugins: [tailwindcss(), sveltekit()],
   test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
+    workspace: [
+      {
+        extends: './vite.config.ts',
+        plugins: [svelteTesting()],
+        test: {
+          name: 'client',
+          environment: 'jsdom',
+          clearMocks: true,
+          include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+          exclude: ['src/lib/server/**', 'src/lib/components/models/**'],
+          setupFiles: ['./vitest-setup-client.ts'],
+        },
+      },
+      {
+        extends: './vite.config.ts',
+        test: {
+          name: 'server',
+          environment: 'node',
+          include: ['src/**/*.{test,spec}.{js,ts}'],
+          exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+        },
+      },
+    ],
+    coverage: {
+      include: ['src/**'],
+      exclude: ['src/**/*.d.ts', 'src/lib/components/models/**'],
+    },
   },
 });
