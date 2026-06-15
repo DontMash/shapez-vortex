2b4b392f1c73e80d139f36a2d57120c40cd40657
Sören Maschmann <smaschmann@slashwhy.de>
2026-06-15 21:28:58 +0200

    chore: remove tailwind config

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
   import { section } from '$lib/components/section';
 </script>
 
-<section class="{section()} prose">
+<section class="{section()} typo">
   <h2 class="divide-foreground flex items-center justify-center divide-x-2">
     <span class="px-6">{page.status}</span>
     <span class="px-6">{page.error?.message}</span>
